82c46041578cac11d649df6317af9ad337c0062a
Muhammed Osman <91621437+Muh-Osman@users.noreply.github.com>
2023-03-31 19:03:00 +0300

    Bug fixes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ select.addEventListener("change", (e) => {
 
 async function getUser(city = "Damascus") {
   try {
-    const response = await axios.get(`http://api.aladhan.com/v1/timingsByCity?country=SY&city=${city}&method=4`);
+    const response = await axios.get(`https://api.aladhan.com/v1/timingsByCity?country=SY&city=${city}&method=4`);
 
     date.innerHTML = `${response.data.data.date.hijri.weekday.ar} ${response.data.data.date.readable}`;
 
@@ -26,6 +26,39 @@ async function getUser(city = "Damascus") {
     asr.innerHTML = response.data.data.timings.Asr;
     maghrib.innerHTML = response.data.data.timings.Maghrib;
     isha.innerHTML = response.data.data.timings.Isha;
+
+    // let d = new Date(); // for now
+    // let hour = d.getHours()
+    // let minute = d.getMinutes() < 10 ? `0${d.getMinutes()}` : d.getMinutes()
+    // let currentTime = +`${hour}${minute}`
+
+    // fajrTime = +response.data.data.timings.Fajr.replace(':','')
+    // sunriseTime = +response.data.data.timings.Sunrise.replace(':','')
+    // dhuhrTime = +response.data.data.timings.Dhuhr.replace(':','')
+    // asrTime = +response.data.data.timings.Asr.replace(':','')
+    // maghribTime = +response.data.data.timings.Maghrib.replace(':','')
+    // ishaTime = +response.data.data.timings.Isha.replace(':','')
+
+
+    // if (currentTime > fajrTime && currentTime < sunriseTime) {
+    //   sunrise.style.backgroundColor = "#eee"
+    // }
+    // if (currentTime > sunriseTime && currentTime < dhuhrTime) {
+    //   dhuhr.style.backgroundColor = "#eee"
+    // }
+    // if (currentTime > dhuhrTime && currentTime < asrTime) {
+    //   asr.style.backgroundColor = "#eee"
+    // }
+    // if (currentTime > asrTime && currentTime < maghribTime) {
+    //   maghrib.style.backgroundColor = "#eee"
+    // }
+    // if (currentTime > maghribTime && currentTime < ishaTime) {
+    //   isha.style.backgroundColor = "#eee"
+    // }
+    // if (currentTime > ishaTime && currentTime < fajrTime) {
+    //   fajr.style.backgroundColor = "#eee"
+    // }
+
 
   } catch (error) {
     console.error(error);
