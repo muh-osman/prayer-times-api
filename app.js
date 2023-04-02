06ba78f41a3390eac1e7dd2fd8c809df20106216
Muhammed Osman <91621437+Muh-Osman@users.noreply.github.com>
2023-04-02 21:58:47 +0300

    Fix

--- app.js
+++ app.js
@@ -8,56 +8,64 @@ const asr = document.getElementById("Asr");
 const maghrib = document.getElementById("Maghrib");
 const isha = document.getElementById("Isha");
 
+// Refetch the date when change the city.
 let city;
 select.addEventListener("change", (e) => {
   city = e.target.value;
-  getUser(city);
+  fetchBasedCity(city);
 });
 
-async function getUser(city = "Damascus") {
+async function fetchBasedCity(city = "Damascus") {
   try {
+    // Fetch
     const response = await axios.get(`https://api.aladhan.com/v1/timingsByCity?country=SY&city=${city}&method=4`);
+    // Show Date under city name
+    let baseDateData = response.data.data.date
+    date.innerHTML = `${baseDateData.hijri.weekday.ar} ${baseDateData.readable}`;
 
-    date.innerHTML = `${response.data.data.date.hijri.weekday.ar} ${response.data.data.date.readable}`;
+    // push data time to HTML page
+    let baseTimeData = response.data.data.timings
+    fajr.innerHTML = baseTimeData.Fajr;
+    sunrise.innerHTML = baseTimeData.Sunrise;
+    dhuhr.innerHTML = baseTimeData.Dhuhr;
+    asr.innerHTML = baseTimeData.Asr;
+    maghrib.innerHTML = baseTimeData.Maghrib;
+    isha.innerHTML = baseTimeData.Isha;
 
-    fajr.innerHTML = response.data.data.timings.Fajr;
-    sunrise.innerHTML = response.data.data.timings.Sunrise;
-    dhuhr.innerHTML = response.data.data.timings.Dhuhr;
-    asr.innerHTML = response.data.data.timings.Asr;
-    maghrib.innerHTML = response.data.data.timings.Maghrib;
-    isha.innerHTML = response.data.data.timings.Isha;
-
-    // let d = new Date(); // for now
-    // let hour = d.getHours()
-    // let minute = d.getMinutes() < 10 ? `0${d.getMinutes()}` : d.getMinutes()
-    // let currentTime = +`${hour}${minute}`
-
-    // fajrTime = +response.data.data.timings.Fajr.replace(':','')
-    // sunriseTime = +response.data.data.timings.Sunrise.replace(':','')
-    // dhuhrTime = +response.data.data.timings.Dhuhr.replace(':','')
-    // asrTime = +response.data.data.timings.Asr.replace(':','')
-    // maghribTime = +response.data.data.timings.Maghrib.replace(':','')
-    // ishaTime = +response.data.data.timings.Isha.replace(':','')
+    // for now
+    let d = new Date();
+    let minute = d.getMinutes()
+    let hour = minute.toString().length < 2 ? d.getHours() *= 10 : d.getHours()
 
 
-    // if (currentTime > fajrTime && currentTime < sunriseTime) {
-    //   sunrise.style.backgroundColor = "#eee"
-    // }
-    // if (currentTime > sunriseTime && currentTime < dhuhrTime) {
-    //   dhuhr.style.backgroundColor = "#eee"
-    // }
-    // if (currentTime > dhuhrTime && currentTime < asrTime) {
-    //   asr.style.backgroundColor = "#eee"
-    // }
-    // if (currentTime > asrTime && currentTime < maghribTime) {
-    //   maghrib.style.backgroundColor = "#eee"
-    // }
-    // if (currentTime > maghribTime && currentTime < ishaTime) {
-    //   isha.style.backgroundColor = "#eee"
-    // }
-    // if (currentTime > ishaTime && currentTime < fajrTime) {
-    //   fajr.style.backgroundColor = "#eee"
-    // }
+    let currentTime = +`${hour}${minute}`
+    
+    fajrTime = +baseTimeData.Fajr.replace(':','')
+    sunriseTime = +baseTimeData.Sunrise.replace(':','')
+    dhuhrTime = +baseTimeData.Dhuhr.replace(':','')
+    asrTime = +baseTimeData.Asr.replace(':','')
+    maghribTime = +baseTimeData.Maghrib.replace(':','')
+    ishaTime = +baseTimeData.Isha.replace(':','')
+
+
+    if (currentTime > fajrTime && currentTime < sunriseTime) {
+      sunrise.classList.add("blink")
+    }
+    if (currentTime > sunriseTime && currentTime < dhuhrTime) {
+      dhuhr.classList.add("blink")
+    }
+    if (currentTime > dhuhrTime && currentTime < asrTime) {
+      asr.classList.add("blink")
+    }
+    if (currentTime > asrTime && currentTime < maghribTime) {
+      maghrib.classList.add("blink")
+    }
+    if (currentTime > maghribTime && currentTime < ishaTime) {
+      isha.classList.add("blink")
+    }
+    if (currentTime > ishaTime || currentTime < fajrTime) {
+      fajr.classList.add("blink")
+    }
 
 
   } catch (error) {
@@ -65,4 +73,4 @@ async function getUser(city = "Damascus") {
   }
 }
 
-getUser();
+fetchBasedCity();
